c1a470e0c0059d748364f779e01aded51fe8f422
Starguy18 <hrouillard@team3467.org>
2016-03-06 09:43:00 -0500

    GSD OI changes and Auto Drive Straight Changes

--- src/org/usfirst/frc/team3467/robot/commands/autonomous/AutoDriveStraight.java
+++ src/org/usfirst/frc/team3467/robot/commands/autonomous/AutoDriveStraight.java
@@ -4,6 +4,9 @@ import org.usfirst.frc.team3467.robot.subsystems.DriveBase.commands.DriveStraigh
 import org.usfirst.frc.team3467.robot.subsystems.DriveBase.commands.ResetDriveEncoders;
 import org.usfirst.frc.team3467.robot.subsystems.Intake.commands.Roller_Actuate;
 import org.usfirst.frc.team3467.robot.subsystems.Shooter.commands.ShooterPrepare;
+import org.usfirst.frc.team3467.robot.subsystems.utilitybar.Pnumatic_system;
+import org.usfirst.frc.team3467.robot.subsystems.utilitybar.commands.Bar_actuate;
+import org.usfirst.frc.team3467.robot.subsystems.utilitybar.commands.Finger_actuate;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,9 +16,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoDriveStraight extends CommandGroup {
     
     public  AutoDriveStraight() {
-    	addSequential(new DriveStraight(100));
+    	addSequential(new DriveStraight(50));
 		addSequential(new Roller_Actuate(true));
 		addSequential(new ResetDriveEncoders());
     	addSequential(new DriveStraight(9900));
+    	addSequential(new Bar_actuate(Pnumatic_system.kIn));
+    	addSequential(new Finger_actuate(Pnumatic_system.kOut));
+    	addSequential(new Bar_actuate(Pnumatic_system.kOut));
     }
 }
