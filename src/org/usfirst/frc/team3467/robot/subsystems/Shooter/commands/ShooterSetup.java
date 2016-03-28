f745e13d51ed6e944387306aefc5286fe6cec214
Scott Kukshtel <scott@kukshtel.com>
2016-03-28 19:49:27 -0400

    Reinstate (& Update) Shooter Changes
    Changes to better handle brownout conditions

--- src/org/usfirst/frc/team3467/robot/subsystems/Shooter/commands/ShooterSetup.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/Shooter/commands/ShooterSetup.java
@@ -4,47 +4,62 @@ import org.usfirst.frc.team3467.robot.commands.CommandBase;
 
 public class ShooterSetup extends CommandBase {
 
-	boolean isClear;
-	boolean latching;
+	boolean isClearing = false;
+	boolean isLatching = false;
+	boolean isBrowned = false;
 	
 	public ShooterSetup() {
 		requires(pultaCat);
+		requires(pneumatics);
 		setTimeout(2);
 	}
 	
 	//Called just before this command runs for the first time 
 	protected void initialize() {
 		pultaCat.cataLatch();
+		pneumatics.compressorStop();
 		pultaCat.initPIDMode();
 		pultaCat.latch();
+		isLatching = true;
 		
-		System.out.println("Shooter Setup: Latching");
-		
-		isClear = false;
-		latching = true;
+		System.out.println("Shooter Setup: Latching");		
 	}
 
 	protected void execute() {
 		
-		if (pultaCat.checkBrownOut()) {
-			System.out.println("Shooter Browned out");
-			end();
+		// Only worry about brownout while latching
+		if (isLatching == true) {
+			pultaCat.checkCurrent();
+			if (pultaCat.checkBrownOut()) {
+				isBrowned = true;
+
+				System.out.println("Shooter Setup: Browned Out!");
+			}
+			
+			if (pultaCat.resetBarIsLatched() || isBrowned) {
+				pultaCat.clear();
+				isLatching = false;
+				isClearing = true;
+				
+				System.out.println("Shooter Setup: Clearing");
+			}
 		}
 		
-		if (pultaCat.checkLatchLimit() || pultaCat.resetBarIsLatched()) {
-			pultaCat.clear();
-			isClear = true;
-			
-			System.out.println("Shooter Setup: Clearing");
-		}
 	}
 
 	protected boolean isFinished() {
-		return (isClear && pultaCat.resetBarIsClear()) || isTimedOut();
+		// Only timeout on the latching portion of setup
+		if ((isLatching && isTimedOut()) || (isClearing && pultaCat.resetBarIsClear())) {
+			System.out.println("Shooter Setup: Finished");
+			return(true);
+		}
+		else
+			return(false);
 	}
 
 	protected void end() {
 		pultaCat.cataStop();
+		pneumatics.compressorStart();
 		pultaCat.initManualMode();
 		System.out.println("Shooter is setup");
 	}
