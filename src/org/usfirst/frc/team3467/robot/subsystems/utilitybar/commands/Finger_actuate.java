e9a02ae8ee231d67fb8d3a6a35468ec99b318582
emartsolf <emartsolf@team3467.org>
2016-03-28 14:56:22 -0400

    Comment Out Finger Actuate

--- src/org/usfirst/frc/team3467/robot/subsystems/utilitybar/commands/Finger_actuate.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/utilitybar/commands/Finger_actuate.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc.team3467.robot.subsystems.utilitybar.commands;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+/*import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 import org.usfirst.frc.team3467.robot.commands.CommandBase;
 
@@ -34,3 +34,4 @@ public class Finger_actuate extends CommandBase {
 		end();
 	}
 }
+*/
