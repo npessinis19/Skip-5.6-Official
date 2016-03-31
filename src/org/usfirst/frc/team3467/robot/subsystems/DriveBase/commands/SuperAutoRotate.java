136cb26bae8e8824b820018fba2dadc09aa6c352
Starguy18 <hrouillard@team3467.org>
2016-03-31 17:00:55 -0400

    Updates (3/31/2016)

--- src/org/usfirst/frc/team3467/robot/subsystems/DriveBase/commands/SuperAutoRotate.java
+++ src/org/usfirst/frc/team3467/robot/subsystems/DriveBase/commands/SuperAutoRotate.java
@@ -21,14 +21,14 @@ public class SuperAutoRotate extends CommandBase {
 	private static double Posit_P = 4.0;
 	private static double Posit_I = 0.0;
 	private static double Posit_D = 0.0;
-	private static double Posit_TOLERANCE = 5.0;
-	double  position = 0.0;
+	private static final double Posit_TOLERANCE = 5.0;
+	private double  position = 0.0;
 	
 	//PID Constants for Angle
 	private static double Gyro_P = 1.0;
 	private static double Gyro_I = 0.001;
 	private static double Gyro_D = 0.0;
-	private static double TOLERANCE = 0.5;
+	private static final double Gyro_TOLERANCE = 0.5;
 	
 	//Management Variables
 	private double angle; //Desired angle as SetPoint
@@ -36,15 +36,20 @@ public class SuperAutoRotate extends CommandBase {
 	private boolean stop = false; //Turn off command when in tolerance zone
 	private int count = 0; //Counter starts when robot is in tolerance zone
 	private static final int sufficientCount = 28; //Once count reaches 28, stop command
-	private double oldGyro_P = Gyro_P;
+	private double oldGyro_P = Gyro_P; //Storing previous value of Gyro_P
+	
+	private static final boolean debugging = true;
 	
 	public SuperAutoRotate(double angle, int mode) {
 		requires(driveBase);
+		
 		buildControllers();
 		setTimeout(20);
 		
 		this.angle = angle;
 		this.mode = mode;
+		
+		this.setInterruptible(true);
 	}
 	
 	public SuperAutoRotate(double angle, int mode, double p, double i, double d) {
@@ -100,10 +105,13 @@ public class SuperAutoRotate extends CommandBase {
 					position = angle * 14.75; //Encoder ticks required to rotate 1 degree
 					double error = GyroPID.getError();
 					
-					if (Math.abs(error) >= 0 || Math.abs(error) <= TOLERANCE) {
+					if (Math.abs(error) >= 0 || Math.abs(error) <= Gyro_TOLERANCE) {
 						if (Gyro_P > 0.1) {
 							Gyro_P = oldGyro_P - 0.3 * (count + 1);
 							if (Gyro_P < 0.01) Gyro_P = 0.01;
+							
+							GyroPID.setPID(Gyro_P, Gyro_I, Gyro_D);
+							oldGyro_P = Gyro_P;
 						}
 						
 						System.out.println("Gyro_P " + Gyro_P);
@@ -132,7 +140,7 @@ public class SuperAutoRotate extends CommandBase {
 							}
 				}});
 		
-		GyroPID.setAbsoluteTolerance(TOLERANCE);
+		GyroPID.setAbsoluteTolerance(Gyro_TOLERANCE);
 		
 		leftpidf_drive = new PIDF_CANTalon("Left Position", driveBase.getLeftTalon(), Posit_TOLERANCE, false, false);
 		leftpidf_drive.setPID(Posit_P, Posit_I, Posit_D);
@@ -159,22 +167,49 @@ public class SuperAutoRotate extends CommandBase {
 		leftpidf_drive.reset();
 	}
 	
+	public void deBug() {
+		if (debugging) {
+			double G_p = SmartDashboard.getNumber("SuperRotate: P");
+			double G_i = SmartDashboard.getNumber("SuperRotate: I");
+			double G_d = SmartDashboard.getNumber("SuperRotate: D");
+			
+			//Manualy Control Gyro_P?
+			//GyroPID.setPID(G_p, G_i, G_d);;
+			GyroPID.setPID(Gyro_P, G_i, G_d);	
+			
+			SmartDashboard.putNumber("SuperRotate: Gyro Error", GyroPID.getError());
+			SmartDashboard.putNumber("SuperRotate", GyroPID.getSetpoint());
+			SmartDashboard.putNumber("SuperRotate: Position", position);
+		}
+	}
+	
+	
 	protected void initialize() {
 		ahrs.gyroReset();
 		driveBase.resetEncoders();
+		
 		driveBase.setSlaveMode(false);
 		driveBase.setControlMode(TalonControlMode.Position);
+		
+		if (debugging) {
+			SmartDashboard.putNumber("SuperRotate: P", Gyro_P);
+			SmartDashboard.putNumber("SuperRotate: I", Gyro_I);
+			SmartDashboard.putNumber("SuperRotate: D", Gyro_D);
+		}
+		
+		stopPID();
 		startPID();
 	}
 
 	protected void execute() {
 		driveBase.reportEncoders();
-		SmartDashboard.putNumber("Gyro Error", GyroPID.getError());
-		SmartDashboard.putNumber("Position (Rotate)", position);
+		ahrs.reportGyroValues();
+		deBug();
 	}
 
 	protected boolean isFinished() {
-		return GyroPID.onTarget() || isTimedOut();
+		//return GyroPID.onTarget() || isTimedOut();
+		return stop;
 	}
 
 	protected void end() {
@@ -185,5 +220,4 @@ public class SuperAutoRotate extends CommandBase {
 	protected void interrupted() {
 		end();
 	}
-
 }
